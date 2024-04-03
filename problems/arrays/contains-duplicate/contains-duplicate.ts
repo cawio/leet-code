adf4313874dcd53866f91a90b05a9442f742c95a
Simon Berner <sberner9@gmail.com>
2024-04-03 09:29:07 +0200

    feat(arrays): Add documentation for containsDuplicate function
    Add documentation for containsDuplicate function

--- problems/arrays/contains-duplicate/contains-duplicate.ts
+++ problems/arrays/contains-duplicate/contains-duplicate.ts
@@ -1,3 +1,9 @@
+/**
+ * Given an integer array nums, return true if any value appears at least twice in the array,
+ * and return false if every element is distinct.
+ *
+ * see: https://leetcode.com/problems/contains-duplicate/
+ */
 export function containsDuplicate(nums: number[]): boolean {
     const seen = new Set<number>();
 
