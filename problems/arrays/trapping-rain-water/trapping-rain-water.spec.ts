b7bd48a666314dfbedcf6290877bbffb7adf5441
Simon Berner <sberner9@gmail.com>
2024-04-03 08:13:07 +0200

    test(arrays): Fix test cases in trapping-rain-water.spec.ts

--- problems/arrays/trapping-rain-water/trapping-rain-water.spec.ts
+++ problems/arrays/trapping-rain-water/trapping-rain-water.spec.ts
@@ -1,27 +1,35 @@
 import { trap } from "./trapping-rain-water";
 
 describe('Arrays: trapping-rain-water', () => {
+    let height: number[];
+    let expectedResult: number;
+
+    beforeEach(() => {
+        height = [];
+        expectedResult = 0;
+    });
+
     test('example 1', () => {
         // Arrange
-        const height = [0,1,0,2,1,0,1,3,2,1,2,1];
-        const expect = 6;
+        height = [0,1,0,2,1,0,1,3,2,1,2,1];
+        expectedResult = 6;
 
         // Act
         const result = trap(height);
 
         // Assert
-        expect(result).toBe(expect);
+        expect(result).toBe(expectedResult);
     });
 
     test('example 2', () => {
         // Arrange
-        const height = [4,2,0,3,2,5];
-        const expect = 9;
+        height = [4,2,0,3,2,5];
+        expectedResult = 9;
 
         // Act
         const result = trap(height);
 
         // Assert
-        expect(result).toBe(expect);
+        expect(result).toBe(expectedResult);
     });
 });
